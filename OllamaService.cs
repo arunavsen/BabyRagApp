53b2bb4eda1cbc4f33c9d297e9509711675c0812
arunavsen <arunavsen96@gmail.com>
2025-05-04 13:25:48 +0600

    Initial commit

---
+++ OllamaService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BabyRagApp
+{
+    public class OllamaService
+    {
+
+        public static async Task<string> AskOllamaAsync(string model, string prompt)
+        {
+            using var client = new HttpClient();
+            var requestBody = new
+            {
+                model = model,
+                prompt = prompt,
+                stream = false
+            };
+
+            string json = JsonSerializer.Serialize(requestBody);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync("http://localhost:11434/api/generate", content);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return $"Error: {response.StatusCode} - {await response.Content.ReadAsStringAsync()}";
+            }
+
+            string responseJson = await response.Content.ReadAsStringAsync();
+            using var doc = JsonDocument.Parse(responseJson);
+            return doc.RootElement.GetProperty("response").GetString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using BabyRagApp;
+
+Console.WriteLine("Ask your question:");
+string prompt = Console.ReadLine();
+
+string response = await OllamaService.AskOllamaAsync("mistral", prompt);
+Console.WriteLine("\nOllama says:\n" + response);
